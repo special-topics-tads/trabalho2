da368e996febeeb1ece67674ebf77b1990888b72
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-04-02 18:28:27 -0300

    routes and middlewares for clients

--- src/middlewares/validateClient.js
+++ src/middlewares/validateClient.js
@@ -1,5 +1,24 @@
 const clientSchema = require('../schemas/clientSchema');
 
-const validateRequest = (req, res, next) => {};
+const validateRequest = (req, res, next) => {
+  if (
+    (req.route.path === '/newClient' || req.route.path === '/updateClient') &&
+    (!req.body.company || !req.body.cnpj || !req.body.address)
+  )
+    return res.status(422).json({ error: 'Campos obrigatórios não preenchidos! ' });
+
+  if (
+    (req.route.path === '/searchClientById' ||
+      req.route.path === '/updateClient' ||
+      req.route.path === '/deleteClient') &&
+    !req.params.id
+  )
+    return res.status(422).json({ error: 'ID inválido!' });
+
+  const { error } = clientSchema.validate(req.body);
+  if (error) return res.status(422).json({ error: error.details });
+
+  next();
+};
 
 module.exports = validateRequest;
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const clientRouter = require('./clientRouter');
+
+router.use('/client', clientRouter);
+
 module.exports = router;
