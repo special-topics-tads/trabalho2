d50ed554e35ab687501ee61ab975083b06a6aeb0
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-04-27 10:15:45 -0300

    fixed port

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const router = require('./routes/router');
+const port = process.env.PORT || process.env.SYSTEM_PORT || 3000;
+
 require('./database');
 
 app.use(express.json());
 app.use(router);
 
-app.listen(process.env.SYSTEM_PORT, () => {
-  console.log('Server running at port: ' + process.env.SYSTEM_PORT);
+app.listen(port, () => {
+  console.log('Server running at port: ' + port);
 });
 
 module.exports = app;
